80ca948a69efa2052318f55723ab95f62d12ea1c
root <root@ip-172-31-35-86.ap-south-1.compute.internal>
2021-04-28 13:27:18 +0000

    find disk utilization and detect file which utilizing disk

---
+++ disk2.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+count=$(df -h |wc -l)
+#var=$(df -h|sed -n "$var"p|awk '{if($5>18 && $6=="/")  print$5}')#to check disk
+#var2=$(sudo cat acd | head -n 3| wc -l)#to count disl uses file line
+for((var=2;var<$count;var++))
+do
+df -h|sed -n "$var"p|awk '{if($5>18 && $6=="/")  print$5 " Disk is consumed by  " $6 " Partition"}'
+var4=$(df -h| sed -n "$var"p |awk '{if($6=="/") print$6}')
+var3=$(df -h|sed -n "$var"p|awk '{if($5>18 && $6=="/")  print$5}')
+if [[ $var3 > 18%  &&  $var4 == / ]]
+then 
+sudo du -s /* |sort -rn>acd
+var2=$(sudo cat acd | head -n 3| wc -l)
+for((var1=1;var1<var2;var1++))
+do
+sudo cat acd|head -n 3|sed -n "$var1"p |awk '{if($1>1000000) print$1 "KB is consumed by  " $2 " Partition"}' 	
+done
+fi
+done
